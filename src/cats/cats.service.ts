2e6f49e6142ef5c2aafd80eef53797eaed6778ce
dvl666 <kamagaspar@gmail.com>
2024-02-08 21:32:45 -0300

    medium commit

---
+++ src/cats/cats.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cat } from './entities/cat.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CatsService {
+
+  constructor(
+    @InjectRepository(Cat)
+    private readonly catRepository: Repository<Cat>
+  ) {}
+
+  create(createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+
+  async findAll() {
+    return await this.catRepository.find();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} cat`;
+  }
+
+  update(id: number, updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} cat`;
+  }
+}
+ 
