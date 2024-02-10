241fcf59e1b72538a06060dc72fe0d2bc6ada842
dvl666 <kamagaspar@gmail.com>
2024-02-10 19:04:44 -0300

    update auth.controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+
+@Controller('auth')
+export class AuthController {
+
+    constructor(
+        private readonly authService: AuthService,
+    ) {}
+
+    @Post('register')
+    
+    register(
+        @Body()
+        registerDto: RegisterDto
+    ) {
+        console.log(registerDto)
+        return this.authService.register(registerDto);
+    }
+
+    @Post('login')
+    login(
+        @Body()
+        loginDto: LoginDto
+    ) {
+        return this.authService.login(loginDto);
+    }
+    
+}
