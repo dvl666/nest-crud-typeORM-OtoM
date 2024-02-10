df7d0cf4662b6dd4c733a6ce0836d3bffa97bf1f
dvl666 <kamagaspar@gmail.com>
2024-02-10 12:55:54 -0300

    g red users --no-spec

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Column, DeleteDateColumn, Entity } from "typeorm";
+
+@Entity()
+export class User {
+
+    @Column({primary:true, generated: true})
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({unique: true})
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({default: 'user'})
+    rol: string
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
