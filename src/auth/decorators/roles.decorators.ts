988f93057cf9f6e801f1b21210060ea46f505c4e
dvl666 <kamagaspar@gmail.com>
2024-02-12 03:15:48 -0300

    update roles.decorators

--- src/auth/decorators/roles.decorators.ts
+++ src/auth/decorators/roles.decorators.ts
@@ -1,3 +1,6 @@
 import { SetMetadata } from "@nestjs/common";
+import { Role } from "../enums/rol.enum";
 
-export const Roles = (role) => SetMetadata( 'roles', role );
+export const ROLES_KEY = 'roles'
+
+export const Roles = (role: Role) => SetMetadata( 'ROLES_KEY', role );
