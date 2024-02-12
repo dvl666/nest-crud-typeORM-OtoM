6b50bde1eac1878ce74ea986d692ae3ad2e83103
dvl666 <kamagaspar@gmail.com>
2024-02-12 03:15:29 -0300

    add roles.decorators

---
+++ src/auth/decorators/auth.decorators.ts
@@ -0,0 +1,14 @@
+import { UseGuards, applyDecorators } from "@nestjs/common";
+import { Role } from "../enums/rol.enum";
+import { AuthGuard } from "../guard/auth.guard";
+import { RolesGuard } from "../guard/roles.guard";
+import { Roles } from "./roles.decorators";
+
+export function Auth(role: Role) {
+
+    return applyDecorators(
+        Roles(role),
+        UseGuards(AuthGuard, RolesGuard)
+    )
+
+}
