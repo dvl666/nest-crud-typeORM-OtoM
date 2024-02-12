92bfd49a0ebfe31e45ee28674eed4cef60c764f2
dvl666 <kamagaspar@gmail.com>
2024-02-12 15:54:53 -0300

    update user.entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -12,7 +12,7 @@ export class User {
     @Column({unique: true})
     email: string;
 
-    @Column()
+    @Column({ nullable: false, select: false })
     password: string;
 
     @Column({default: 'user'})
